e11793bb959071bd1e0062f395d8763d4a7eb35f
Seb <72100sj@gmail.com>
2026-08-15 14:11:19 +0200

    Add new exercises to ExercicesDB
    Added new exercises including Cobra and Chien tête en bas with images.

--- js/data.js
+++ js/data.js
@@ -1,47 +1,58 @@
-// DONNÉES DES EXERCICES (V1.4)
-// Correction du texte pour YTW appliquée.
-
 const ExercicesDB = [
     {
         id: "exo_pompes",
         name: "Pompes",
-        emoji: "💪",
+        image: "img/pompes.jpg", 
         duration: 30,
         description: "Mains écartées largeur d'épaules, corps gainé. Descendre jusqu'à frôler le sol."
     },
     {
         id: "exo_squat",
         name: "Squats",
-        emoji: "🦵",
+        image: "img/squat.jpg",
         duration: 30,
         description: "Poids sur les talons, dos droit, descendre les hanches."
     },
     {
         id: "exo_fentes",
         name: "Fentes Alternées",
-        emoji: "🚶",
+        image: "img/fentes.jpg",
         duration: 40,
         description: "Un grand pas en avant, le genou arrière frôle le sol."
     },
     {
         id: "exo_gainage",
         name: "Gainage Planche",
-        emoji: "🪵",
+        image: "img/gainage.jpg",
         duration: 45,
         description: "Sur les coudes, corps aligné, contracter les abdos."
     },
     {
         id: "exo_ytw",
         name: "YTW",
-        emoji: "🔠",
+        image: "img/ytw.jpg",
         duration: 45,
         description: "Position debout. Buste incliné vers l'avant avec charnière de hanches. Dos maintenu dans une position stable. Réalisation des mouvements de bras correspondant aux positions Y, T et W."
     },
     {
         id: "exo_jumping",
         name: "Jumping Jacks",
-        emoji: "🤸",
+        image: "img/jumping.jpg",
         duration: 30,
         description: "Sauts avec écartement des bras et des jambes synchronisé."
+    },
+    {
+        id: "exo_cobra",
+        name: "Cobra",
+        image: "img/cobra.jpg",
+        duration: 30,
+        description: "Posture de yoga. Allongé sur le ventre, redressez le buste."
+    },
+    {
+        id: "exo_chien_bas",
+        name: "Chien tête en bas",
+        image: "img/chien.jpg",
+        duration: 30,
+        description: "Posture de yoga. Formez un V inversé avec votre corps."
     }
 ];
