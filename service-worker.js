4af188eaf5553549b8eac85e80c06c924f8caa60
Seb <72100sj@gmail.com>
2026-08-15 22:25:52 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,46 @@
-const CACHE_NAME = 'ninja-force-v1';
+const CACHE_NAME = 'ninja-force-v2';
 const ASSETS = [
     './',
     './index.html',
     './css/style.css',
-    './js/storage.js',
-    './js/exercises.js',
-    './js/workout.js',
+    './js/data.js',
     './js/app.js',
-    './manifest.json'
+    './manifest.json',
+    './assets/icons/icon-192.png',
+    './assets/icons/icon-512.png',
+    './assets/exercises/01-squat.jpg',
+    './assets/exercises/02-fentes.jpg',
+    './assets/exercises/03-pont.jpg',
+    './assets/exercises/04-pompes.jpg',
+    './assets/exercises/05-dips.jpg',
+    './assets/exercises/06-birddog.jpg',
+    './assets/exercises/07-ytw.jpg',
+    './assets/exercises/08-planche.jpg',
+    './assets/exercises/09-planchelat.jpg',
+    './assets/exercises/10-deadbug.jpg',
+    './assets/exercises/11-pike.jpg',
+    './assets/exercises/12-commando.jpg',
+    './assets/exercises/13-mollets.jpg',
+    './assets/exercises/14-climbers.jpg',
+    './assets/exercises/15-jacks.jpg',
+    './assets/exercises/16-genoux.jpg'
 ];
 
 self.addEventListener('install', (e) => {
     e.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames
+                    .filter((name) => name !== CACHE_NAME)
+                    .map((name) => caches.delete(name))
+            );
+        })
     );
 });
 
