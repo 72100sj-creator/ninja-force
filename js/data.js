b96cab7b9e30dda9516c9f01f53feab7cbd2f56d
Seb <72100sj@gmail.com>
2026-08-15 14:18:04 +0200

    Update exercise images and add new exercises

--- js/data.js
+++ js/data.js
@@ -1,58 +1,115 @@
+// DONNÉES DES EXERCICES - NINJA FORCE
 const ExercicesDB = [
-    {
-        id: "exo_pompes",
-        name: "Pompes",
-        image: "img/pompes.jpg", 
-        duration: 30,
-        description: "Mains écartées largeur d'épaules, corps gainé. Descendre jusqu'à frôler le sol."
-    },
     {
         id: "exo_squat",
         name: "Squats",
-        image: "img/squat.jpg",
+        image: "assets/exercises/01-squat.jpg",
         duration: 30,
         description: "Poids sur les talons, dos droit, descendre les hanches."
     },
     {
         id: "exo_fentes",
         name: "Fentes Alternées",
-        image: "img/fentes.jpg",
+        image: "assets/exercises/02-fentes.jpg",
         duration: 40,
         description: "Un grand pas en avant, le genou arrière frôle le sol."
     },
     {
-        id: "exo_gainage",
-        name: "Gainage Planche",
-        image: "img/gainage.jpg",
-        duration: 45,
-        description: "Sur les coudes, corps aligné, contracter les abdos."
+        id: "exo_pont",
+        name: "Pont fessier",
+        image: "assets/exercises/03-pont.jpg",
+        duration: 30,
+        description: "Allongé sur le dos, pieds à plat, monter le bassin en contractant les fessiers."
+    },
+    {
+        id: "exo_pompes",
+        name: "Pompes",
+        image: "assets/exercises/04-pompes.jpg",
+        duration: 30,
+        description: "Mains écartées largeur d'épaules, corps gainé. Descendre jusqu'à frôler le sol."
+    },
+    {
+        id: "exo_dips",
+        name: "Dips",
+        image: "assets/exercises/05-dips.jpg",
+        duration: 30,
+        description: "Mains sur un support stable, fléchir les bras pour descendre les fesses."
+    },
+    {
+        id: "exo_birddog",
+        name: "Bird Dog",
+        image: "assets/exercises/06-birddog.jpg",
+        duration: 40,
+        description: "En appui quadrupedique, tendre simultanément le bras opposé et la jambe opposée."
     },
     {
         id: "exo_ytw",
         name: "YTW",
-        image: "img/ytw.jpg",
+        image: "assets/exercises/07-ytw.jpg",
         duration: 45,
         description: "Position debout. Buste incliné vers l'avant avec charnière de hanches. Dos maintenu dans une position stable. Réalisation des mouvements de bras correspondant aux positions Y, T et W."
     },
     {
-        id: "exo_jumping",
+        id: "exo_planche",
+        name: "Gainage Planche",
+        image: "assets/exercises/08-planche.jpg",
+        duration: 45,
+        description: "Sur les coudes, corps aligné, contracter les abdos."
+    },
+    {
+        id: "exo_planchelat",
+        name: "Planche Latérale",
+        image: "assets/exercises/09-planchelat.jpg",
+        duration: 30,
+        description: "En appui sur un seul coude et sur le côté des pieds, corps bien aligné."
+    },
+    {
+        id: "exo_deadbug",
+        name: "Dead Bug",
+        image: "assets/exercises/10-deadbug.jpg",
+        duration: 40,
+        description: "Sur le dos, descendre alternativement un bras et une jambe tout en gardant le bas du dos collé au sol."
+    },
+    {
+        id: "exo_pike",
+        name: "Pike Push-ups",
+        image: "assets/exercises/11-pike.jpg",
+        duration: 30,
+        description: "Pompes position piquée, bassin vers le haut pour cibler les épaules."
+    },
+    {
+        id: "exo_commando",
+        name: "Commando",
+        image: "assets/exercises/12-commando.jpg",
+        duration: 30,
+        description: "En position de planche, descendre sur un coude puis l'autre, et remonter."
+    },
+    {
+        id: "exo_mollets",
+        name: "Mollets",
+        image: "assets/exercises/13-mollets.jpg",
+        duration: 30,
+        description: "Montées sur la pointe des pieds pour travailler les mollets."
+    },
+    {
+        id: "exo_climbers",
+        name: "Mountain Climbers",
+        image: "assets/exercises/14-climbers.jpg",
+        duration: 30,
+        description: "En position de planche haute, ramener alternativement les genoux vers la poitrine à bon rythme."
+    },
+    {
+        id: "exo_jacks",
         name: "Jumping Jacks",
-        image: "img/jumping.jpg",
+        image: "assets/exercises/15-jacks.jpg",
         duration: 30,
         description: "Sauts avec écartement des bras et des jambes synchronisé."
     },
     {
-        id: "exo_cobra",
-        name: "Cobra",
-        image: "img/cobra.jpg",
+        id: "exo_genoux",
+        name: "Montées de genoux",
+        image: "assets/exercises/16-genoux.jpg",
         duration: 30,
-        description: "Posture de yoga. Allongé sur le ventre, redressez le buste."
-    },
-    {
-        id: "exo_chien_bas",
-        name: "Chien tête en bas",
-        image: "img/chien.jpg",
-        duration: 30,
-        description: "Posture de yoga. Formez un V inversé avec votre corps."
+        description: "Courir sur place en montant les genoux haut vers la poitrine."
     }
 ];
